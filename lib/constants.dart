85a98a25b0a52db563bc48b9f06b90f0045b318e
Nipun VV <nipunvv@gmail.com>
2021-09-04 00:15:06 +0530

    Setup theme colors and custom texts

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xff323143);
+Color secondaryColor = Color(0xff303043);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:bucket_list/constants.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,12 +9,13 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('BucketList'),
+        backgroundColor: primaryColor,
       ),
       body: SafeArea(
         child: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          color: Colors.white,
+          color: secondaryColor,
         ),
       ),
     );
